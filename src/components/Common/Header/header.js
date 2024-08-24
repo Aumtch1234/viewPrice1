e0ab34eae78bf091d8639294074e56f79828ad1e
ZoRo <taweechok.k@ku.th>
2024-08-24 22:55:14 +0700

    Commit 4 || Time 3:30:46 s.

--- src/components/Common/Header/header.js
+++ src/components/Common/Header/header.js
@@ -8,7 +8,7 @@ function Header() {
   return (
     <div className='navbar'>
       <h1 className='logo'>
-        ViewPrice<span style={{ color: "var(--grey)" }}>.</span>
+        ViewPrice<span style={{ color: "var(--blue)" }}>.</span>
       </h1>
       <div className='links'>
         <Link to='/'>
